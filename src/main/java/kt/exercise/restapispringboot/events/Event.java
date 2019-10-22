83369ac2fcb11c5b69c4836059693c01b6dbd57c
Park Ki Tae <ktpark@modoric.com>
2019-10-22 16:00:49 +0900

    POST /api/events returns 201

--- src/main/java/kt/exercise/restapispringboot/events/Event.java
+++ src/main/java/kt/exercise/restapispringboot/events/Event.java
@@ -2,12 +2,19 @@ package kt.exercise.restapispringboot.events;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Builder @AllArgsConstructor @NoArgsConstructor
-@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
 public class Event {
 
+    @Id @GeneratedValue
     private Integer id;
     private String name;
     private String description;
@@ -21,6 +28,7 @@ public class Event {
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+    @Enumerated(EnumType.STRING)
     private EventStatus eventStatus;
 
 }
---
+++ src/main/java/kt/exercise/restapispringboot/events/EventRepository.java
@@ -0,0 +1,7 @@
+package kt.exercise.restapispringboot.events;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, Integer> {
+
+}
