b13261a3e2ba7cbf28f15da8bb07886097efa7f2
Park Ki Tae <ktpark@modoric.com>
2019-10-22 16:55:40 +0900

    fail on unknown properties

--- src/test/java/kt/exercise/restapispringboot/events/EventControllerTests.java
+++ src/test/java/kt/exercise/restapispringboot/events/EventControllerTests.java
@@ -35,6 +35,34 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = new EventDto().builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018,11,23,14,21))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018,11,24,14,21))
+                .endEventDateTime(LocalDateTime.of(2018,11,26,14,21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타텁 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+        .andExpect(jsonPath("id").exists())
+        .andExpect(header().exists(HttpHeaders.LOCATION))
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+        .andExpect(jsonPath("id").value(Matchers.not(100)))
+        .andExpect(jsonPath("free").value(Matchers.not(true)))
+        .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)));
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = new Event().builder()
                 .id(100)
                 .name("Spring")
@@ -56,13 +84,10 @@ public class EventControllerTests {
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-        .andExpect(jsonPath("id").exists())
-        .andExpect(header().exists(HttpHeaders.LOCATION))
-        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-        .andExpect(jsonPath("id").value(Matchers.not(100)))
-        .andExpect(jsonPath("free").value(Matchers.not(true)))
-        .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)));
+                .andExpect(status().isBadRequest())
+
+                ;
+
     }
 
 
